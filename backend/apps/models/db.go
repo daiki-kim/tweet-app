40b9e1f8113319eb0eff210fb9569410c4d0cc2a
daiki-kim <kimdaiki5021@gmail.com>
2024-08-13 10:17:51 +0900

    add setup gorm.DB with MySQL or SqLite

---
+++ backend/apps/models/db.go
@@ -0,0 +1,61 @@
+package models
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/daiki-kim/tweet-app/backend/configs"
+	"gorm.io/driver/mysql"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+const (
+	InstanceSqLite int = iota
+	InstanceMySQL
+)
+
+var (
+	DB                            *gorm.DB
+	errInvalidSQLDatabaseInstance = errors.New("invalid sql db instance")
+)
+
+func GetModels() []interface{} {
+	return []interface{}{
+		&User{},
+	}
+}
+
+// create new database
+func NewDatabaseFactory(instance int) (db *gorm.DB, err error) {
+	switch instance {
+	case InstanceMySQL:
+		dsn := fmt.Sprintf(
+			"%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True",
+			configs.Config.DBUser,
+			configs.Config.DBPassword,
+			configs.Config.DBHost,
+			configs.Config.DBPort,
+			configs.Config.DBName,
+		)
+		db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	case InstanceSqLite:
+		db, err = gorm.Open(sqlite.Open(configs.Config.DBName), &gorm.Config{})
+	default:
+		return nil, errInvalidSQLDatabaseInstance
+	}
+
+	return db, err
+}
+
+// initialize database
+func SetDatabase(instance int) (err error) {
+	db, err := NewDatabaseFactory(instance)
+	if err != nil {
+		return err
+	}
+
+	DB = db
+	return err
+}
