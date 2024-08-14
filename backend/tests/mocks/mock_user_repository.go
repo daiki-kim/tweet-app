72a6e20fb882dd3004dbdb1f45c5c8db1dee8a77
daiki-kim <kimdaiki5021@gmail.com>
2024-08-14 09:09:24 +0900

    add user repository mock

---
+++ backend/tests/mocks/mock_user_repository.go
@@ -0,0 +1,23 @@
+package mocks
+
+import (
+	"github.com/daiki-kim/tweet-app/backend/apps/models"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockUserRepository struct {
+	mock.Mock
+}
+
+func (m *MockUserRepository) CreateUser(user *models.User) error {
+	args := m.Called(user)
+	return args.Error(0)
+}
+
+func (m *MockUserRepository) FindUserByEmail(email string) (*models.User, error) {
+	args := m.Called(email)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*models.User), args.Error(1)
+}
