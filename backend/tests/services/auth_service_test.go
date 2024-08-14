7afc4e15af2f76082dfbfd7f80a6f24d11e4f883
daiki-kim <kimdaiki5021@gmail.com>
2024-08-14 09:09:02 +0900

    add signup test

---
+++ backend/tests/services/auth_service_test.go
@@ -0,0 +1,123 @@
+package services_test
+
+import (
+	"errors"
+	"log"
+	"testing"
+	"time"
+
+	"github.com/daiki-kim/tweet-app/backend/apps/models"
+	"github.com/daiki-kim/tweet-app/backend/apps/services"
+	"github.com/daiki-kim/tweet-app/backend/tests/mocks"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func TestPrepareBaseUserModel(t *testing.T) {
+	// ユーザーモデルを準備
+	name := "testuser"
+	email := "test@example.com"
+	dobString := "2020-01-01"
+	dob := time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC)
+
+	user, err := services.PrepareBaseUserModel(name, email, dobString)
+
+	assert.NoError(t, err)
+	assert.Equal(t, name, user.Name)
+	assert.Equal(t, email, user.Email)
+	assert.Equal(t, dob, user.Dob)
+}
+
+func TestSignupUsingOAuthSuccess(t *testing.T) {
+	// モックレポジトリを準備
+	mockRepo := &mocks.MockUserRepository{}
+	testAuthService := services.NewAuthService(mockRepo)
+
+	// ユーザーモデルを準備
+	name := "testuser"
+	email := "test@example.com"
+	dobString := "2020-01-01"
+	dob := time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC)
+
+	expectedUser := &models.User{
+		Name:  name,
+		Email: email,
+		Dob:   dob,
+	}
+
+	// SignupUsingOAuthで使用するmockメソッドを準備
+	mockRepo.On("CreateUser", mock.MatchedBy(func(user *models.User) bool {
+		return user.Name == expectedUser.Name && user.Email == expectedUser.Email && user.Dob == expectedUser.Dob
+	})).Return(nil)
+
+	// サインアップ
+	err := testAuthService.SignupUsingOAuth(name, email, dobString)
+
+	assert.NoError(t, err)
+	mockRepo.AssertExpectations(t)
+}
+
+func TestSignupUsingOAuthErrorByNonEmail(t *testing.T) {
+	mockRepo := &mocks.MockUserRepository{}
+	testAuthService := services.NewAuthService(mockRepo)
+
+	// emailが入力されていないユーザーモデルを準備
+	name := "testuser"
+	email := ""
+	dobString := "2020-01-01"
+	dob := time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC)
+
+	wrongUser := &models.User{
+		Name:  name,
+		Email: email,
+		Dob:   dob,
+	}
+
+	// SignupUsingOAuthで使用するmockメソッドを準備
+	mockRepo.On("CreateUser", wrongUser).Return(errors.New("email is required"))
+
+	// サインアップ
+	err := testAuthService.SignupUsingOAuth(name, email, dobString)
+
+	assert.Error(t, err)
+	mockRepo.AssertExpectations(t)
+}
+
+func TestSignupSuccess(t *testing.T) {
+	// モックレポジトリを準備
+	mockRepo := &mocks.MockUserRepository{}
+	testAuthService := services.NewAuthService(mockRepo)
+
+	// ユーザーモデルを準備
+	name := "testuser"
+	email := "test@example.com"
+	password := "testpassword"
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	hashedPasswordString := string(hashedPassword)
+	dobString := "2020-01-01"
+	dob := time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC)
+
+	expectedUser := &models.User{
+		Name:     name,
+		Email:    email,
+		Password: hashedPasswordString,
+		Dob:      dob,
+	}
+
+	log.Println(hashedPassword)
+
+	// SignupUsingOAuthで使用するmockメソッドを準備
+	mockRepo.On("CreateUser", mock.MatchedBy(func(user *models.User) bool {
+		return user.Name == expectedUser.Name &&
+			user.Email == expectedUser.Email &&
+			bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)) == nil &&
+			user.Dob == expectedUser.Dob
+	})).Return(nil)
+
+	// サインアップ
+	err := testAuthService.Signup(name, email, password, dobString)
+
+	assert.NoError(t, err)
+	mockRepo.AssertExpectations(t)
+}
