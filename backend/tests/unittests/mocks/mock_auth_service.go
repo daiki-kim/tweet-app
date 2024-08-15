e57362c03efd22800d967db2daa8f6d851ddca1a
daiki-kim <kimdaiki5021@gmail.com>
2024-08-16 07:23:33 +0900

    add test for login using oauth service

--- backend/tests/unittests/mocks/mock_auth_service.go
+++ backend/tests/unittests/mocks/mock_auth_service.go
@@ -1,6 +1,9 @@
 package mocks
 
-import "github.com/stretchr/testify/mock"
+import (
+	"github.com/daiki-kim/tweet-app/backend/apps/services"
+	"github.com/stretchr/testify/mock"
+)
 
 type MockAuthService struct {
 	mock.Mock
@@ -14,4 +17,12 @@ func (m *MockAuthService) SignupUsingOAuth(name, email, dobString string) error 
 func (m *MockAuthService) Signup(name, email, dobString, password string) error {
 	args := m.Called(name, email, dobString, password)
 	return args.Error(0)
+}
+
+func (m *MockAuthService) LoginUsingOAuth(email string) (*services.LoginResponse, error) {
+	args := m.Called(email)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*services.LoginResponse), args.Error(1)
 }
