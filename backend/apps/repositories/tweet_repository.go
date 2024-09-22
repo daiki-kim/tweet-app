af4f14057a588ffcb130a86ebea67c92d654a2b2
daiki-kim <kimdaiki5021@gmail.com>
2024-09-22 17:20:49 +0900

    Add get tweet

--- backend/apps/repositories/tweet_repository.go
+++ backend/apps/repositories/tweet_repository.go
@@ -1,6 +1,7 @@
 package repositories
 
 import (
+	"errors"
 	"log"
 
 	"github.com/daiki-kim/tweet-app/backend/apps/models"
@@ -9,6 +10,8 @@ import (
 
 type ITweetRepository interface {
 	CreateTweet(tweet *models.Tweet) error
+	GetTweet(id uint) (*models.Tweet, error)
+	GetUserTweets(userId uint) ([]*models.Tweet, error)
 }
 
 type TweetRepository struct {
@@ -28,4 +31,31 @@ func (r *TweetRepository) CreateTweet(tweet *models.Tweet) error {
 
 	log.Println("created tweet: ", tweet)
 	return nil
+}
+
+func (r *TweetRepository) GetTweet(id uint) (*models.Tweet, error) {
+	var tweet models.Tweet
+
+	result := r.DB.First(&tweet, "id = ?", id)
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		log.Println("tweet not found: ", result.Error)
+		return nil, errors.New("tweet not found")
+	} else if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return &tweet, nil
+}
+
+func (r *TweetRepository) GetUserTweets(userId uint) ([]*models.Tweet, error) {
+	var tweets []*models.Tweet
+
+	result := r.DB.Where("user_id = ?", userId).Find(&tweets)
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		return nil, errors.New("tweets not found")
+	} else if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return tweets, nil
 }
