718b2e63bc5c3bce7feb84c78a7c6d3c50444516
daiki-kim <kimdaiki5021@gmail.com>
2024-09-23 15:31:57 +0900

    Add DeleteFollow and GetFollow

--- backend/apps/models/follower_model.go
+++ backend/apps/models/follower_model.go
@@ -2,8 +2,8 @@ package models
 
 type Follower struct {
 	ID         uint `gorm:"primaryKey;autoIncrement" json:"id"`
-	FollowerID uint `gorm:"not null" json:"follower_id"`
-	FolloweeID uint `gorm:"not null" json:"followee_id"`
+	FollowerID uint `gorm:"not null" json:"follower_id"` // followしている人
+	FolloweeID uint `gorm:"not null" json:"followee_id"` // followされている人
 
 	// relations
 	// Follower, Followee情報をFollowerデータと一緒に取得したい場合はPerloadを使用する
--- backend/apps/repositories/follower_repository.go
+++ backend/apps/repositories/follower_repository.go
@@ -2,7 +2,6 @@ package repositories
 
 import (
 	"errors"
-	"log"
 
 	"github.com/daiki-kim/tweet-app/backend/apps/models"
 	"gorm.io/gorm"
@@ -10,7 +9,9 @@ import (
 
 type IFollowerRepository interface {
 	CreateFollower(follower *models.Follower) (*models.Follower, error)
+	GetFollower(id uint) (*models.Follower, error)
 	GetFollowers(followeeId uint) ([]*models.Follower, error)
+	DeleteFollower(id uint) error
 }
 
 type FollowerRepository struct {
@@ -30,6 +31,16 @@ func (r *FollowerRepository) CreateFollower(follower *models.Follower) (*models.
 	return follower, nil
 }
 
+func (r *FollowerRepository) GetFollower(id uint) (*models.Follower, error) {
+	var follower models.Follower
+	result := r.DB.First(&follower, "id = ?", id)
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		return nil, errors.New("follower not found")
+	}
+
+	return &follower, nil
+}
+
 func (r *FollowerRepository) GetFollowers(followeeId uint) ([]*models.Follower, error) {
 	var followers []*models.Follower
 	result := r.DB.Preload("Follower").Where("followee_id = ?", followeeId).Find(&followers)
@@ -39,11 +50,16 @@ func (r *FollowerRepository) GetFollowers(followeeId uint) ([]*models.Follower, 
 		return nil, result.Error
 	}
 
-	for _, follower := range followers {
-		log.Println("[GetFollowers] follower: ", follower)
-		log.Println("[GetFollowers] &follower: ", &follower)
-		log.Println("[GetFollowers] *follower: ", *follower)
+	return followers, nil
+}
+
+func (r *FollowerRepository) DeleteFollower(id uint) error {
+	result := r.DB.Delete(&models.Follower{}, "id = ?", id)
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		return errors.New("follower not found")
+	} else if result.Error != nil {
+		return result.Error
 	}
 
-	return followers, nil
+	return nil
 }
